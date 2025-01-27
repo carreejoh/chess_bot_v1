ae59902a52243ff8990d21686d88f6e0e9e4957c
Carter Johnson <carreejoh4637@gmail.com>
2025-01-27 14:02:49 -0700

    tailwind works

---
+++ chess_bot_v1/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="bg-gray-300">
+      <h2 className="text-2xl">fgsdf</h2>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
